c92af791a80c3f21a031b69431fcc1491c73c751
Alexandre Bustico <alexandre.bustico@enac.fr>
2018-04-02 13:57:19 +0200

    reduce heap size

--- mcuconf.h
+++ mcuconf.h
@@ -264,7 +264,7 @@
  */
 #define STM32_WDG_USE_IWDG                  FALSE
 
-#define CH_HEAP_SIZE (32*1024)
+#define CH_HEAP_SIZE (1024)
 #define CH_HEAP_USE_TLSF 0 // if 0 or undef, chAlloc will be used
 
 #endif /* MCUCONF_H */
